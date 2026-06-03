254196e79c6598a3791e89e2065e7342741b2308
Yug Thakral <yugthakral07@gmail.com>
2026-06-04 01:02:15 +0530

    fetching data through api

---
+++ script.js
@@ -0,0 +1,27 @@
+let searchbox=document.querySelector('.searchbox');
+let searchbutton=document.querySelector('.searchbutton');
+let recipeContainer=document.querySelector('.recipe-container');
+
+const fetchRecipes=async(query)=>{
+    recipeContainer.innerHTML='fetching recipes...';
+    const data=await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response=await data.json();
+   recipeContainer.innerHTML='';
+    response.meals.forEach(meal => {
+        const recieveDiv=document.createElement('div');
+        recieveDiv.classList.add('recipe');
+        recieveDiv.innerHTML=`
+         <img src="${meal.strMealThumb}">
+            <h2>${meal.strMeal}</h2>
+            <p>${meal.strArea}</p>
+            <p>${meal.strCategory}</p>
+        `;
+        recipeContainer.appendChild(recieveDiv);
+    });
+    
+};
+searchbutton.addEventListener('click',(e)=>{
+      e.preventDefault();
+      const query=searchbox.value.trim();
+      fetchRecipes(query);
+})
